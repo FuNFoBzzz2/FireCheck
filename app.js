75215f2a99afb72390c06dad1c882450ccc825c7
Delau Kursovuiy <167048920+FuNFoBzzz2@users.noreply.github.com>
2025-05-15 17:44:07 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-analytics.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAv69ST8kfulsnpKoB3Qv-d5tWvwE6s1sk",
@@ -13,4 +12,15 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+//import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-analytics.js";
+//const analytics = getAnalytics(app);
+
+// Проверяем, не блокируется ли Analytics перед инициализацией
+if (typeof window.gtag === 'function') {
+    import("https://www.gstatic.com/firebasejs/11.7.3/firebase-analytics.js")
+        .then(({ getAnalytics }) => {
+            const analytics = getAnalytics(app);
+        })
+        .catch(e => console.log("Analytics blocked:", e));
+}
