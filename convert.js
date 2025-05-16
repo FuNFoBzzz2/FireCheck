6d5c8d66b6ba17873ae56935948f101b14cce616
Delau Kursovuiy <167048920+FuNFoBzzz2@users.noreply.github.com>
2025-05-17 02:47:13 +0700

    Add files via upload

--- convert.js
+++ convert.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getDatabase, ref, set, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js";
+
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAv69ST8kfulsnpKoB3Qv-d5tWvwE6s1sk",
@@ -15,11 +19,58 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app); 
 
-const email = document.getElementById('mail').value;
-const password = document.getElementById('password').value;
 
-const submit = document.getElementById('submit');
-submit.addEventListener('click', function (event){
-  event.preventDefault()
-  alert(5)
+const submitreg = document.getElementById('submit-reg');
+submitreg.addEventListener('click', function (event){
+    event.preventDefault()
+    const email = document.getElementById('mail').value;
+    const pas1 = document.getElementById('password-1').value;
+    const pas2 = document.getElementById('password-2').value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(email)) {
+        if(pas1.length>6 || pas2.length>6){
+            if(pas1==pas2){
+                const auth = getAuth();
+                createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    window.location.href = "./home.html";
+                    alert("Creating Account");
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    alert(errorMessage);
+                });
+            } else{
+                alert("Пароли не совпадают!");
+            }
+        } else{
+            alert("Пароль должен содержать не менее 6 символов!")
+        } 
+    }else{
+        alert("Введите корректный email! Пример: user@example.com"); 
+    }
+});
+
+const submitsign = document.getElementById('submit-sign');
+submitsign.addEventListener('click', function (event){
+    event.preventDefault()
+    const email = document.getElementById('mail').value;
+    const password = document.getElementById('password').value;
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        
+        const user = userCredential.user;
+        window.location.href = "./home.html";
+        alert("Creating Account");
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+        
+        
+    
 });
