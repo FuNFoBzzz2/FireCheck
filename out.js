ea29d56772221bc4987a9ee4b881a997dd3b8499
Delau Kursovuiy <167048920+FuNFoBzzz2@users.noreply.github.com>
2025-05-17 03:34:05 +0700

    Add files via upload

--- out.js
+++ out.js
@@ -1,3 +1,4 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 const firebaseConfig = {
